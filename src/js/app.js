eb6233c4ad12c4c7995e7a525161363861ccc952
der benutzer <claudio.steffen@gmx.net>
2016-09-23 22:43:10 +0200

    more groundwork

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,19 @@
 $( document ).ready(function() {
-    $("h1").text("jquery");
+
+    $("h1").text("My Notes");
+
+    var notelist = [
+        {id:01, date:'01.01.1990', title: 'do stuff', finished:false, content:"I need to do something"},
+        {id:02, date:'01.01.1990', title: 'do stuff', finished:false, content:"I need to do something"},
+        {id:03, date:'01.01.1990', title: 'do stuff', finished:false, content:"I need to do something"},
+        {id:04, date:'01.01.1990', title: 'do stuff', finished:false, content:"I need to do something"}
+        ];
+
+    var noteListTemplateText = $('#noteListTemplateText').html();
+    var createNoteListHtml = Handlebars.compile (noteListTemplateText);
+    var renderData = function () {
+        console.log("render");
+        $(document.body).append (createNoteListHtml (notelist));
+    };
+    renderData();
 });
