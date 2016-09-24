0be271a19cbd0d4ae70bff5aac83d22859d2f158
der benutzer <claudio.steffen@gmx.net>
2016-09-24 22:51:50 +0200

    Adding Create Node Dialog Filling in more of the structure

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,62 @@
 $( document ).ready(function() {
 
-    $("h1").text("My Notes");
+    //jqueryui initialization
+    $( "#datepicker" ).datepicker();
+    $( "#importanceInput input" ).checkboxradio();
+    $( "#importanceInput" ).controlgroup();
 
-    var notelist = [
-        {id:01, date:'01.01.1990', title: 'do stuff', finished:false, content:"I need to do something"},
-        {id:02, date:'01.01.1990', title: 'do stuff', finished:false, content:"I need to do something"},
-        {id:03, date:'01.01.1990', title: 'do stuff', finished:false, content:"I need to do something"},
-        {id:04, date:'01.01.1990', title: 'do stuff', finished:false, content:"I need to do something"}
-        ];
 
+    //notelist JSON
+    var noteData = {"notelist":[
+        {id:1, date:'01.01.1990',created:'01.01.1990', title: 'do stuff', finished:0, content:"I need to do something",importance:0},
+        {id:2, date:'01.01.1990',created:'01.01.1990', title: 'do stuff', finished:0, content:"I need to do something",importance:0},
+        {id:3, date:'01.01.1990',created:'01.01.1990', title: 'do stuff', finished:0, content:"I need to do something",importance:0},
+        {id:4, date:'01.01.1990',created:'01.01.1990', title: 'do stuff', finished:0, content:"I need to do something",importance:0}
+        ]};
+
+    //save data to element
+    //ToDo : use localStorage or Server
+    $('#data').data('notelist',noteData);
+
+
+    //handlebars renderData
     var noteListTemplateText = $('#noteListTemplateText').html();
     var createNoteListHtml = Handlebars.compile (noteListTemplateText);
     var renderData = function () {
-        console.log("render");
-        $(document.body).append (createNoteListHtml (notelist));
+        //get data from element
+        //ToDo : use localStorage or Server
+        var notelist = $('#data').data('notelist').notelist;
+        $('#mainContent').append (createNoteListHtml (notelist));
     };
+
+    var addNote = function (note) {
+        var notelist = $('#data').data('notelist').notelist;
+        note.id=(notelist.length+1);
+        notelist.push(note);
+        $('#data').data('notelist',{"notelist":notelist});
+    };
+
+    $('#createNote').click(function(){
+        openNoteDialog();
+    });
+
+    $('#createNoteCancel').click(function(e){
+        e.preventDefault();
+        closeNoteDialog();
+    });
+
+    var openNoteDialog = function(){
+        $('#mainContent').hide();
+        $('#createNoteDialogContainer').show();
+    }
+
+    var closeNoteDialog = function(){
+        $('#createNoteDialogContainer').hide();
+        $('#mainContent').show();
+    }
+
+    //test addNote
+    addNote({id:05, date:'01.01.1991',created:'01.01.1990', title: 'do stuff', finished:0, content:"I need to do something",importance:0});
+
     renderData();
 });
