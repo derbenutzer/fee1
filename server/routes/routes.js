9180c177f76c250aa3fc8853826f71a6df9d3e00
der benutzer <claudio.steffen@gmx.net>
2016-12-03 10:52:59 +0100

    serve static file create package.json with dependencies

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var router = express.Router();
+
 var notes = require('../controller/notes.js');
 
-router.get('/', function(request, response){
-    response.sendFile("src/index.html", {"root":__dirname.replace("\\server\\routes","")});
-});
 router.get("/get", notes.getNotes);
 router.post("/store", notes.storeNotes);
 
